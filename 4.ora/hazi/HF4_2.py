7875b37fc301a05846a240ab148f7fb9ef4de347
DomVikRo <viktordomotor@gmail.com>
2021-10-06 12:57:08 +0200

    python

--- 4.ora/hazi/HF4_2.py
+++ 4.ora/hazi/HF4_2.py
@@ -1,12 +1,10 @@
-try:
-    a1 = int(input("Kérem az első elemet: "))
-    a2 = int(input("Kérem a második elemet: "))
-    n=int(input("Hányadik elemnek számoljam ki az értékét: "))
-except ValueError:
-    print("Hiba a beolvasás során")
-q=float(a2/a1)
+a1=int(input("Kérem az első számot: "))
+a2=int(input("Kérem a második számot: "))
+n=int(input("Hányadik elem érdekel? "))
+q=a2/a1
 m=a2*q
-i=2
-while i<=n:
+i=3
+while i<n:
     m=m*q
-print("A felhasznó által megadott elem értéke: "+m)
+    i+=1
+print(m)
